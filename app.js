aefef190916de34ca97501b246761833e8e00106
Abhishek Nair <uabhisheknair2001@gmail.com>
2024-05-04 17:03:40 +0530

    first commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var path = require('path');
-var express = require('express');
+var path = require("path");
+var express = require("express");
 
 var app = express();
 
-var staticPath = path.join(__dirname, '/public');
+var staticPath = path.join(__dirname, "/public");
 app.use(express.static(staticPath));
 
-app.listen(3000, () => console.log('Server has started in 3000'));
+app.listen(8661, () => console.log("Server has started in 8661"));
